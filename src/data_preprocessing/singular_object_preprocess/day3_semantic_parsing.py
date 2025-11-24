5d5f9ecd8cb8c59cffa4ff9a046129aa7840b0f2
Kaylie Stuteville <140630741+Kstuteville@users.noreply.github.com>
2025-11-24 15:17:08 -0500

    changes

--- src/data_preprocessing/singular_object_preprocess/day3_semantic_parsing.py
+++ src/data_preprocessing/singular_object_preprocess/day3_semantic_parsing.py
@@ -12,7 +12,7 @@ Day 3 — Map PartNet Part Labels to Human-Readable Names (Chair 331)
 import os, json, numpy as np, open3d as o3d
 import matplotlib.pyplot as plt, matplotlib.patches as mpatches
 
-base_dir = "/Volumes/T7/data_v0/49513"
+base_dir = "/Volumes/T7/data_v0/48732"
 points_file = os.path.join(base_dir, "point_sample", "sample-points-all-pts-nor-rgba-10000.txt")
 labels_file = os.path.join(base_dir, "point_sample", "sample-points-all-label-10000.txt")
 json_file = os.path.join(base_dir, "result_after_merging.json")
